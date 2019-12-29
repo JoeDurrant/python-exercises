15460cf78acc189efe27de220a1d176e5ee8208e
JoeDurrant <josephwdurrant@gmail.com>
2019-12-29 00:12:06 +0000

    Added input checking to fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,16 @@
 #Performs fizzbuzz on numbers up to specified limit, default is 50
 def fizzbuzz(limit = 50):
-    for i in range(1,limit+1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+    try:
+        for i in range(1,limit+1):
+            if i % 3 == 0 and i % 5 == 0:
+                print("FizzBuzz")
+            elif i % 3 == 0:
+                print("Fizz")
+            elif i % 5 == 0:
+                print("Buzz")
+            else:
+                print(i)
+    except TypeError:
+        print("TypeError: Your input should be an integer!")
 
-fizzbuzz()
+fizzbuzz("Hello")
