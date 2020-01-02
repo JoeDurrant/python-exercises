f0431e10f54ba3a873eb8f22d76e29de802996a9
JoeDurrant <josephwdurrant@gmail.com>
2020-01-02 23:01:17 +0000

    Fix cows and bulls incorrect counts

--- cows_and_bulls.py
+++ cows_and_bulls.py
@@ -1,12 +1,11 @@
 import random
 def cows_and_bulls():
-    #number = str(random.randint(1000,9999))
-    number = str(1211) #Testing edge cases
+    number = str(random.randint(1000,9999))
     guessed = False
     while guessed == False:
         cow_count = 0
         bull_count = 0
-        guess = input("Enter your guess (4 digit number): ")
+        guess = input("Enter your guess: ")
         if len(guess) != 4 or not guess.isdigit():
             print("Your guess has to be a 4 digit number!")
         elif guess == number:
@@ -14,15 +13,12 @@ def cows_and_bulls():
             guessed = True
         else:
             already_guessed = [] # Track which digits have already been checked, stops cows being recounted when counting bulls
-            # Check cow count
+            # Check cow and bull count
             for i in range(4):
                 if guess[i] == number[i]:
                     cow_count += 1
                     already_guessed.append(guess[i]) # Fixes bull count
-
-            # Check bull count
-            for i in range(4):
-                if guess[i] != number[i] and guess[i] not in already_guessed and guess[i] in number:
+                elif guess[i] != number[i] and guess[i] not in already_guessed and guess[i] in number:
                     already_guessed.append(guess[i])
                     bull_count += 1
             print(str(cow_count) + " cow(s), " + str(bull_count) + " bull(s)")
